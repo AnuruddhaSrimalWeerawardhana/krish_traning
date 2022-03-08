123f89d6b6b2b0bfc934c5de7fc06ab182ce4dcc
AnuruddhaSrimalWeerawardhana <54682768+AnuruddhaSrimalWeerawardhana@users.noreply.github.com>
2022-03-08 11:54:53 +0530

    Add files via upload

--- CCH_Mising_Number/src/main/java/mising_number.java
+++ CCH_Mising_Number/src/main/java/mising_number.java
@@ -1,1 +1,35 @@
+import java.util.*;
+import java.util.Arrays;
 
+
+public class mising_number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] number = new int[5];
+        System.out.println("Enter the Number");
+        for (int i=0;i<number.length;i++){
+            number[i] = input.nextInt();
+        }
+        int temp;
+        for (int i= 0;i< number.length; i++){
+            for(int j=i; j< number.length; j++){
+                if(number[i] > number[j]){
+                    temp = number[i];
+                    number[i] = number[j];
+                    number[j] = temp;
+                }
+            }
+        }
+        System.out.println("Missing Number is:" + getMissingNumber(number));
+    }
+
+    public static int getMissingNumber(int arr[])
+    {
+        int i, total;
+        int n = arr.length;
+        total = (n + 1) * (n + 2) / 2;
+        for (i = 0; i < n; i++)
+            total -= arr[i];
+        return total;
+    }
+}
